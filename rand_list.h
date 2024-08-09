a5ccb709e4bb78cbe7e5f9f2db461b118e00158a
sevenninesixthree <1875488550@qq.com>
2024-08-09 18:28:56 +0800

    fix template error

--- rand_list.h
+++ rand_list.h
@@ -1,24 +1,71 @@
 #ifndef RAND_LIST
 #define RAND_LIST
 
+#include <cstddef>
+#include <cstring>
 template<class DATA>
 class rand_list{
 private:
   DATA *list;int length,N;
-  bool expend();
-  bool expend(int nAdd);
-  void pureAdd(DATA value);
+  bool expend(){
+    if(list==NULL)return 1;
+    DATA* temp;temp=new DATA[(N+1)*2];
+    memcpy(temp,list,sizeof(DATA)*length);
+    delete[] list;N=(N+1)*2-1;list=temp;
+    return 0;
+  }
+  bool expend(int nAdd){
+    if(list==NULL)return 1;
+    DATA *temp;temp=new DATA[N+1+nAdd];
+    memcpy(temp,list,sizeof(DATA)*length);
+    delete[] list;N+=nAdd;list=temp;
+    return 0;
+  }
+  void pureAdd(DATA value){
+    list[length++]=value;return;
+  }
 public:
-  rand_list(int n);
-  rand_list();
-  ~rand_list();
-  rand_list& operator=(const rand_list &_in);
-  bool add(DATA value);
-  bool add(DATA value,int delta);
-  bool del(unsigned long long int seed);
-  DATA select(unsigned long long int seed);
-  operator int();
-  void clear();
+  rand_list(int n){
+    length=0;list=new DATA[n];N=n-1;
+  }
+  rand_list(){
+    length=0;list=NULL;N=0;
+  }
+  ~rand_list(){
+    delete[] list;
+  }
+  rand_list& operator=(const rand_list &_in){
+    if(&_in==this)return *this;
+    if(_in.list==NULL){length=0;list=NULL;N=0;return *this;}
+    length=_in.length;N=_in.N;list=new DATA[N+1];
+    memcpy(list,_in.list,sizeof(DATA)*(N+1));
+    return *this;
+  }
+  bool add(DATA value){
+    if(length>N&&expend()==1)
+      return 1;
+    pureAdd(value);return 0;
+  }
+  bool add(DATA value,int delta){
+    if(length>N&&expend(delta)==1)
+      return 1;
+    pureAdd(value);return 0;
+  }
+  bool del(unsigned long long int seed){
+    if(length<=0)return 1;
+    int temp=length;
+    list[seed%temp]=list[--length];
+    return 0;
+  }
+  DATA select(unsigned long long int seed){
+    return list[seed%length];
+  }
+  operator int(){
+    return length;
+  }
+  void clear(){
+    length=0;
+  }
 };
 
 #endif
