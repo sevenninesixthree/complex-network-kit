3c96c21cb0796afbd5e86e1f355979973290beb5
sevenninesixthree <1875488550@qq.com>
2025-01-27 20:54:13 +0800

    add template to draw eps picture with latex support for gnuplot

---
+++ gnuEpsLatex.sh
@@ -0,0 +1,16 @@
+#!/bin/gnuplot
+#input picName
+picName="test"
+#logName="'/dev/null'"
+set term epslatex color standalone size 6,5
+set output "tmp.tex"
+
+# your code (in default just a command test)
+test
+
+set output
+!latex tmp.tex
+!dvips -o @picName.eps tmp.dvi
+!xelatex tmp.tex
+!cp tmp.pdf @picName.pdf
+!rm tmp*
